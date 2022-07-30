96a2f906ae0bbf02c9edf715d70707d7b1ed37ca
lucqaz01 <Lucassobczak3@gmail.com>
2022-07-30 10:24:26 -0400

    Clean and start
    I cleaned up the old code from the skeleton I am using and have begun program architecture.

--- app.py
+++ app.py
@@ -1,119 +1,73 @@
-# @Email:  contact@pythonandvba.com
-# @Website:  https://pythonandvba.com
-# @YouTube:  https://youtube.com/c/CodingIsFun
-# @Project:  Sales Dashboard w/ Streamlit
-
-
-
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
+import time
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="PO Tracker", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 @st.cache
 def get_data_from_excel():
     df = pd.read_excel(
-        io="supermarkt_sales.xlsx",
+        io="po_priority.xlsx",
         engine="openpyxl",
-        sheet_name="Sales",
-        skiprows=3,
-        usecols="B:R",
-        nrows=1000,
+        sheet_name="sku",
+        skiprows=0,
+        usecols="A:AR",
+        nrows=2000,
     )
     # Add 'hour' column to dataframe
-    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    #df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     return df
 
 df = get_data_from_excel()
 
+
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
-city = st.sidebar.multiselect(
-    "Select the City:",
-    options=df["City"].unique(),
-    default=df["City"].unique()
+mcat = st.sidebar.multiselect(
+    "Select MCAT:",
+    options=df["MCAT"].unique(),
+    default=None,
 )
 
-customer_type = st.sidebar.multiselect(
-    "Select the Customer Type:",
-    options=df["Customer_type"].unique(),
-    default=df["Customer_type"].unique(),
+purchase_order = st.sidebar.multiselect(
+    "Select PO#:",
+    options=df["po"].unique().round(),
+    default=None,
 )
 
-gender = st.sidebar.multiselect(
-    "Select the Gender:",
-    options=df["Gender"].unique(),
-    default=df["Gender"].unique()
+
+vendor_name = st.sidebar.multiselect(
+    "Select Vendor:",
+    options=df["Vendor"].unique(),
+    default=None,
 )
 
 df_selection = df.query(
-    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+    "MCAT == @mcat & po ==@purchase_order & Vendor ==@vendor_name"
 )
+# *** MAIN *** 
 
-# ---- MAINPAGE ----
-st.title(":bar_chart: Sales Dashboard")
-st.markdown("##")
+# ---PO Views---
 
-# TOP KPI's
-total_sales = int(df_selection["Total"].sum())
-average_rating = round(df_selection["Rating"].mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
-average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+#Based off query selection, fetch and update chart to display the total number of ordered units delivered
+selection_po_qty_total = int(df_selection["po_qty"].sum())
+selection_po_delivered_total = int(df_selection["qty_delivered"].sum())
+selection_po_total = float(selection_po_delivered_total/ selection_po_qty_total)*100
 
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
-    st.subheader("Total Sales:")
-    st.subheader(f"US $ {total_sales:,}")
-with middle_column:
-    st.subheader("Average Rating:")
-    st.subheader(f"{average_rating} {star_rating}")
-with right_column:
-    st.subheader("Average Sales Per Transaction:")
-    st.subheader(f"US $ {average_sale_by_transaction}")
 
-st.markdown("""---""")
+# PO Status by MCAT
 
-# SALES BY PRODUCT LINE [BAR CHART]
-sales_by_product_line = (
-    df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
-)
-fig_product_sales = px.bar(
-    sales_by_product_line,
-    x="Total",
-    y=sales_by_product_line.index,
-    orientation="h",
-    title="<b>Sales by Product Line</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-    template="plotly_white",
-)
-fig_product_sales.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=(dict(showgrid=False))
-)
-
-# SALES BY HOUR [BAR CHART]
-sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
-fig_hourly_sales = px.bar(
-    sales_by_hour,
-    x=sales_by_hour.index,
-    y="Total",
-    title="<b>Sales by hour</b>",
-    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-    template="plotly_white",
-)
-fig_hourly_sales.update_layout(
-    xaxis=dict(tickmode="linear"),
-    plot_bgcolor="rgba(0,0,0,0)",
-    yaxis=(dict(showgrid=False)),
-)
-
+mcat_po_status_delivered = df.groupby("MCAT")["qty_delivered"].sum()
+mcat_po_status_total = (mcat_po_status_delivered / df.groupby("MCAT")["po_qty"].sum().round())*100
 
 left_column, right_column = st.columns(2)
-left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-right_column.plotly_chart(fig_product_sales, use_container_width=True)
+with left_column:
+    st.subheader("PO Percentage Delivered by MCAT")
+    st.write(selection_po_total)
+
+# Calendar
 
 
 # ---- HIDE STREAMLIT STYLE ----
